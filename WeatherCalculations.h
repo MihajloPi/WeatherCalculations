32b7c769fdb8831999f264d0dda5cac137589210
MihajloPi <mihajlo2003petkovic@users.noreply.github.com>
2023-11-29 16:36:17 +0100

    Fixing header file

--- WeatherCalculations.h
+++ WeatherCalculations.h
@@ -8,9 +8,9 @@ class Weather {
 public:
   const char forecast[27][36] = { "Settled fine", "Fine weather", "Becoming fine", "Fine, becoming less settled", "Fine, possible showers", "Fairly fine, improving", "Fairly fine, possible showers early", "Fairly fine, showery later", "Showery early, improving", "Changeable, mending", "Fairly fine, showers likely", "Rather unsettled clearing later", "Unsettled, probably improving", "Showery, bright intervals", "Showery, becoming less settled", "Changeable, some rain", "Unsettled, short fine intervals", "Unsettled, rain later", "Unsettled, some rain", "Mostly very unsettled", "Occasional rain, worsening", "Rain at times, very unsettled", "Rain at frequent intervals", "Rain, very unsettled", "Stormy, may improve", "Stormy, much rain" };
   // equivalents of Zambretti 'dial window' letters A - Z
-  static const uint8_t rise_options[22] = { 25, 25, 25, 24, 24, 19, 16, 12, 11, 9, 8, 6, 5, 2, 1, 1, 0, 0, 0, 0, 0, 0 };
-  static const uint8_t steady_options[22] = { 25, 25, 25, 25, 25, 25, 23, 23, 22, 18, 15, 13, 10, 4, 1, 1, 0, 0, 0, 0, 0, 0 };
-  static const uint8_t fall_options[22] = { 25, 25, 25, 25, 25, 25, 25, 25, 23, 23, 21, 20, 17, 14, 7, 3, 1, 1, 1, 0, 0, 0 };
+  const uint8_t rise_options[22] = { 25, 25, 25, 24, 24, 19, 16, 12, 11, 9, 8, 6, 5, 2, 1, 1, 0, 0, 0, 0, 0, 0 };
+  const uint8_t steady_options[22] = { 25, 25, 25, 25, 25, 25, 23, 23, 22, 18, 15, 13, 10, 4, 1, 1, 0, 0, 0, 0, 0, 0 };
+  const uint8_t fall_options[22] = { 25, 25, 25, 25, 25, 25, 25, 25, 23, 23, 21, 20, 17, 14, 7, 3, 1, 1, 1, 0, 0, 0 };
 
   enum WindDirection { N,
                        NNE,
@@ -27,7 +27,8 @@ public:
                        W,
                        WNW,
                        NW,
-                       NNW };
+                       NNW,
+                       NOW};
 
 
   // Correction factors for wind direction in the northern hemisphere
@@ -47,7 +48,8 @@ public:
     { W, -0.03 },
     { WNW, -0.005 },
     { NW, 0.015 },
-    { NNW, 0.03 }
+    { NNW, 0.03 },
+	  { NOW, 0.0 }
   };
 
   // Correction factors for wind direction in the southern hemisphere
@@ -67,7 +69,8 @@ public:
     { E, -0.045 },
     { ESE, -0.03 },
     { SE, -0.005 },
-    { SSE, 0.015 }
+    { SSE, 0.015 },
+	  { NOW, 0.0 }
   };
 
 
@@ -81,7 +84,7 @@ public:
   double getHumidex(double Temperature, double DewPoint);
   double getWindChill(double Temperature, double WindSpeed);
   uint8_t getComfort(double heatIndex);
-  constexpr uint16_t getAQI(uint16_t PM25, uint16_t PM10);
+  uint16_t getAQI(uint16_t PM25, uint16_t PM10);
   //Pressure trend variable: 1 for rising, 2 for falling, everything else is by default steady
   //Months are 1-12
   //Wind direction is descriptive ("N", "SSE" etc.); if there's no wind, you should send nonesense string like "NOW" (NO Wind)
